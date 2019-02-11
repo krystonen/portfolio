bccdf2cc5d57ec49f00ccef4af9ddc872ca11acc
Krisztina Kovari <krystonen@gmail.com>
2019-02-11 12:34:25 +0100

    Intergrated new and create actions for portfolios page

--- app/controllers/catalogs_controller.rb
+++ app/controllers/catalogs_controller.rb
@@ -2,4 +2,22 @@ class CatalogsController < ApplicationController
   def index
     @catalog_items = Catalog.all
   end
+
+  def new
+    @catalog_item = Catalog.new
+  end
+
+  def create
+    @catalog_item = Catalog.new(params.require(:catalog).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @catalog_item.save
+        format.html { redirect_to catalogs_path, notice: 'Your portfolio item is now live.' }
+        # format.json { render :show, status: :created, location: @catalog }
+      else
+        format.html { render :new }
+        # format.json { render json: @catalog.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
