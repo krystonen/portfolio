71491f25ed0ee8f7adf086dd59eadd259854315e
Krisztina Kovari <krystonen@gmail.com>
2019-02-12 12:56:47 +0100

    Implemented concern for managing image data

--- app/models/catalog.rb
+++ app/models/catalog.rb
@@ -1,4 +1,5 @@
 class Catalog < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   def self.angular
@@ -6,10 +7,12 @@ class Catalog < ApplicationRecord
   end
 
   scope :ruby_on_rails_portfolio_items,  -> {where(subtitle: "Ruby on Rails")}
+
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||= "https://placehold.it/600x400"
-    self.thumb_image ||= "https://placehold.it/350x200"
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
+
 end
