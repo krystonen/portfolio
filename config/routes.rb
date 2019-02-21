751a0bb23bf800c5ef71d4c78ca1a77df9393fcb
Krisztina Kovari <krystonen@gmail.com>
2019-02-21 13:32:27 +0100

    Customized registration, login, logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-
-  devise_for :users
+  devise_for :users, path:'', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   root to: 'pages#home'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
