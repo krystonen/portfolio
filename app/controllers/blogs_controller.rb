485646f306d389b7583a8c7781d1b895c0f340b2
Krisztina Kovari <krystonen@gmail.com>
2019-02-26 23:05:09 +0100

    Updated auth to include draft and pubished status toggle

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,7 +2,7 @@ class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
   layout "blog"
   access all: [:show, :index],
-         user: {except: [:destroy, :new, :create, :update, :edit]},
+         user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]},
          site_admin: :all
 
   # GET /blogs
@@ -69,10 +69,10 @@ class BlogsController < ApplicationController
   end
 
   def toggle_status
-    if @blog.draft?
-      @blog.published!
-     elsif @blog.published?
-      @blog.draft!
+    if @blog.Draft?
+      @blog.Published!
+     elsif @blog.Published?
+      @blog.Draft!
     end
     redirect_to blogs_url, notice: "Post status has been updated."
   end
