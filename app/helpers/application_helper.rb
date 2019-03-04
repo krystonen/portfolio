d3082fcccb0ae8b0464dddfbcf2bd82907d4b87b
Krisztina Kovari <krystonen@gmail.com>
2019-03-04 16:28:54 +0100

    Refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -60,4 +60,15 @@ module ApplicationHelper
   def active? path
     "active" if current_page? path
   end
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, :title => "Krisztina Kovari Portfolio", sticky: false, time: 2000)
+  end
 end
