43479e5a754c1280f3674d5743fc7ccf7329c14c
Krisztina Kovari <krystonen@gmail.com>
2019-03-04 19:27:34 +0100

    Implemented initial connection to Twitter API

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ Bundler.require(*Rails.groups)
 
 module Portfolio
   class Application < Rails::Application
+    config.eager_load_paths << "#{Rails.root}/lib"
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
