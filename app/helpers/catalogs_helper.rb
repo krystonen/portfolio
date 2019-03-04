2f3a8d0b67392137f102922975d2e353f7fb8ee4
Krisztina Kovari <krystonen@gmail.com>
2019-03-04 14:01:58 +0100

    Reset seed file, moved placeholder logic into view

--- app/helpers/catalogs_helper.rb
+++ app/helpers/catalogs_helper.rb
@@ -1,2 +1,15 @@
 module CatalogsHelper
+  def image_generator(height:, width:)
+    "https://placehold.it/#{height}x#{width}"
+  end
+
+  def catalog_img img, type
+    if img
+      img
+    elsif type == 'thumb'
+      image_generator(height: '350', width: '200')
+    else
+      image_generator(height: '600', width: '400')
+    end
+  end
 end
--- app/models/concerns/placeholder.rb
+++
@@ -1,7 +0,0 @@
-module Placeholder
-  extend ActiveSupport::Concern
-
-  def self.image_generator(height:, width:)
-    "https://placehold.it/#{height}x#{width}"
-  end
-end
