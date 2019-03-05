a0ed21de587cb9c3bc7b8bf63488b236beee7c0d
Krisztina Kovari <krystonen@gmail.com>
2019-03-05 11:01:59 +0100

    Created a comment boradcast configured the websocket route and updated the comment model with a callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :comments
   devise_for :users, path:'', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
 
   root to: 'pages#home'
@@ -19,4 +18,6 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
+
+  mount ActionCable.server => '/cable'
 end
