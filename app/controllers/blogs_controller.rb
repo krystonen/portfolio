58e6f50c6efb5ec5f9ed11ccf1c736c51e2215d1
Krisztina Kovari <krystonen@gmail.com>
2019-03-05 14:45:15 +0100

    Limited access to draft blogs to site-admin only

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -19,10 +19,14 @@ class BlogsController < ApplicationController
   # GET /blogs/1
   # GET /blogs/1.json
   def show
-     @blog = Blog.includes(:comments).friendly.find(params[:id])
-     @comment = Comment.new
-     @page_title = @blog.title
-     @seo_keywords = @blog.body
+    if logged_in?(:site_admin) || @blog.published?
+      @blog = Blog.includes(:comments).friendly.find(params[:id])
+      @comment = Comment.new
+      @page_title = @blog.title
+      @seo_keywords = @blog.body
+    else
+      redirect_to blogs_path, notice: "You are not authorized to access this page."
+    end
   end
 
   # GET /blogs/new
